a3831fb21d03c41e5f4faacb216f03f775d8c581
Francesco Pira <dev@fpira.com>
2025-11-25 18:16:42 +0100

    feat: skip existing rows

--- src/process.py
+++ src/process.py
@@ -27,18 +27,28 @@ CREATE TABLE IF NOT EXISTS ocr_data (
 conn.commit()
 
 
+def file_exists_in_db(filename):
+    cursor.execute('SELECT 1 FROM ocr_data WHERE filename = ?', (filename,))
+    return cursor.fetchone() is not None
+
 def save_to_db(filename, text):
-    cursor.execute('INSERT OR REPLACE INTO ocr_data (filename, ocr_text) VALUES (?, ?)', (filename, text))
-    conn.commit()
+    if not file_exists_in_db(filename):
+        cursor.execute('INSERT INTO ocr_data (filename, ocr_text) VALUES (?, ?)', (filename, text))
+        conn.commit()
+        return True
+    return False
 
 def process_images(image_folder):
     for filename in os.listdir(image_folder):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
             full_path = os.path.join(image_folder, filename)
             print(f"Processing {filename}...")
+
             ocr_text = pytesseract.image_to_string(Image.open((full_path)))
-            save_to_db(filename, ocr_text)
-            print(f"OCR saved for {filename}")
+            if save_to_db(filename, ocr_text):
+                print(f"OCR saved for {filename}")
+            else:
+                print(f"{filename} already exists in the database. Skipping.")
 
 if __name__ == "__main__":
     SCREENSHOTS_DIR = "C:\\Users\\francesco\\OneDrive - Leonardo S.p.a. - Div Cyber Security\\Screenshots"
